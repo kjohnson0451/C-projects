e50b03d74804cf4ccd4c4699a94295f12b246a9a
kjohnson0451 <kjohnson0451@gmail.com>
2016-02-03 14:00:23 -0500

    Updating wordRank documentation.

--- wordRank/wordRank.c
+++ wordRank/wordRank.c
@@ -5,6 +5,8 @@
  * For every string S, there are n other permutations of S. If all of those
  * permutations were put in alphabetical order, then S would fall under
  * a certain rank in that order. wordRank.c determines which rank that is.
+ *
+ * For example, AB would fall under rank 1, BA would fall under rank 2. 
  * 
  * usage: ./wordRank <string>
  *
